5d037c22b18c3aa3aab844203633bc20a65f836e
Salo <salomonco@gmail.com>
2020-08-31 20:56:26 -0400

    more comments

--- hwk1.py
+++ hwk1.py
@@ -5,12 +5,15 @@ salary = int(input("How much money do you make?"))
 spending = int (input("How much do you spend per month?"))
 saving = salary - spending
 
+#This line will tell you if you are saving enough money to eventually reach "financial freedom"
 if spending >= salary * 0.9:
     print("You are spending too much money")
 else:
     print("Good job, you are on the path to financial freedom")
 
 print("You are currently saving " + str(saving) + " dollars per month")
+
+#This code will help you calculate wether you are making enough money to save X ammount in 10 years
 
 future = int(input("How much money do you want to have in your savings in 10 years? (Please include only a number)"))
 
