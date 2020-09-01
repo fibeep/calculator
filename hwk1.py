8fd7249f45a22296d849cc46c14624da8dbcf462
Salo <salomonco@gmail.com>
2020-08-31 20:53:59 -0400

    upgrade 2

--- hwk1.py
+++ hwk1.py
@@ -7,3 +7,11 @@ if spending >= salary * 0.9:
 else:
     print("Good job, you are on the path to financial freedom")
 
+print("You are currently saving " + str(saving) + " dollars per month")
+
+future = int(input("How much money do you want to have in your savings in 10 years? (Please include only a number)"))
+
+if future / 10 <= salary:
+    print("You need to save",future/10,"dollars every year from now on.")
+else:
+    print("You need to find a new job that pays" + str(future/10) + "because with your current salary it is impossible to save this much money")
